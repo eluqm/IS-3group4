c287c38d5f7a7a8f98e420b07f4cd8db8f351877
KodingT <kodingtutoriales@gmail.com>
2022-07-01 15:28:02 -0500

    feat: added link and school database migrations

---
+++ database/migrations/2022_07_01_195809_create_links_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 300);
+            $table->string('description', 3000);
+            $table->string('url', 1500);
+            $table->integer('school_id');
+            $table->timestamps();
+            $table->integer('made_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
