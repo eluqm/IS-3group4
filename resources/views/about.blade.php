9a5d132fd73f4f33008eb63ba73527cb18727831
KodingT <kodingtutoriales@gmail.com>
2022-07-11 11:20:41 -0500

    feat: added About view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.services')
+
+@section('title')
+    Acerca
+@endsection
+
+@section('scripts')
+@endsection
+
+@section('styles')
+@endsection
+
+@section('content')
+
+<div class="container">
+    <br>
+    <div class="alert alert-primary" role="alert">
+      <h1 class="display-3" align="center">Acerca</h1>
+
+      <h2>Tatleon</h2>
+      <p>
+        Tatleon es un sitio web que permite a los usuarios tanto estudiantes como docentes de la Universidad Nacional de San Agustín, 
+        poder acceder a una serie de servicios muy útiles para un mejor desenvolvimiento en la Universidad, de poder crear 
+        recursos y modificarlos.
+        Además de extender este acceso en modo de solo lectura para cualquier persona que pueda acceder a la página.
+      </p>
+      <h2>Servicios</h2>
+      <p>
+        Tatleon ofrece sus servicios de forma gratuita para que los estudiantes y docentes de la Universidad Nacional de San Agustín
+        y cualquier otra persona puedan acceder a ellas libremente y puedan aportar tanto en cuanto puedan con este proyecto.
+      </p>
+    </div>
+
+    <br>
+    <div class="alert alert-primary" role="alert">
+      <h2 align="center"> Equipo de Trabajo</h2>
+      <h4>The Delta Team</h4>
+      <p>
+        Este proyecto fue desarrollado por los estudiantes miembros de "The Delta Team" de la carrera de Ciencias de la Computación
+        de la Universidad Nacional de San Agustín, el cual se empezó en el primer semestre del año 2022.
+      </p>
+    </div>
+
+
+@endsection
