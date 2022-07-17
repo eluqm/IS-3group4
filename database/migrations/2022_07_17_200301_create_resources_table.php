4a0168f549f057f319b86cc4f744ac385fa1512c
VanessaMMH <mahua.ea@gmail.com>
2022-07-17 16:19:09 -0500

    feat: added Resources migration

---
+++ database/migrations/2022_07_17_200301_create_resources_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->string('description', 3000);
+            $table->string('url', 1500);
+            $table->integer('course_id');
+            $table->timestamps();
+            $table->integer('made_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+    }
+}
