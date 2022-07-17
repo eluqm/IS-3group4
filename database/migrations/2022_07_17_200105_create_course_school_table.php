65007810761f32ebbb2cf94eba4961e9668c82db
VanessaMMH <mahua.ea@gmail.com>
2022-07-17 16:18:11 -0500

    feat: added Course School migration

---
+++ database/migrations/2022_07_17_200105_create_course_school_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseSchoolTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_school', function (Blueprint $table) {
+            //id must not autoincrement
+            $table->integer('id')->unsigned();
+            $table->primary('id');
+            $table->integer('course_id');
+            $table->integer('school_id');
+            $table->unique(['course_id', 'school_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_school');
+    }
+}
