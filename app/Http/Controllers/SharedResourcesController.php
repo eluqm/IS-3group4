05dd752a2c278578da96c47a234f18ceb1a1f2c6
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2022-07-18 09:21:54 -0500

    feat: get courses by school

--- app/Http/Controllers/SharedResourcesController.php
+++ app/Http/Controllers/SharedResourcesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Resource;
 use App\Models\Course;
 use App\Models\CourseSchool;
@@ -14,7 +15,22 @@ class SharedResourcesController extends Controller
     public function index()
     {
         $courses = Course::all();
-        $origin = 'Todos los cursos';
-        return view('shared_resources', compact('courses', 'origin'));
+        $schools = School::all();
+        
+        return view('shared_resources', compact('courses', 'schools'));
+    }
+    public function by_school(Request $request)
+    {
+        $school_id = $request->input('school');
+        $school = School::where('id', $school_id)->get()->first();
+        $courses = DB::table('courses')
+            ->join('course_school', 'courses.id', '=', 'course_school.course_id')
+            ->select('courses.*')
+            ->where('course_school.school_id', $school_id)
+            ->orderBy('courses.name', 'asc')
+            ->get();
+
+        $origin = $school->name;
+        return view('shared_resources', compact('courses', 'school', 'origin'));
     }
 }
