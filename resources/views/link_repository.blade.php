dc168caf8967efa416e7be5263e5d95f324d5ff8
KodingT <kodingtutoriales@gmail.com>
2022-07-18 12:36:47 -0500

    fix: menu dropdowns not working

--- resources/views/link_repository.blade.php
+++ resources/views/link_repository.blade.php
@@ -5,61 +5,58 @@
 @endsection
 
 @section('scripts')
-    <script src="{{ asset(mix('js/app.js')) }}" defer></script>
 @endsection
 
 @section('styles')
-    <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
-    <link rel="stylesheet" href="{!! asset('css/link_repository.css') !!}">
-    <link rel="stylesheet" href="{!! asset('css/navbar.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/services.css') !!}">
 @endsection
 
 @section('content')
 
     <div class="container">
 
-    @if (isset($schools))
-    <table class="table table-hover table-bordered">
-        <thead>
-            </tr>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nombre</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            if ($schools != null) 
-            {
-                foreach ($schools as $school) 
+        @if (isset($schools))
+        <table class="table table-hover table-bordered">
+            <thead>
+                </tr>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($schools != null) 
                 {
-                    echo "<tr>";
-                    echo "<th scope=\"row\">" . $school->id . "</th>";
-                    echo "<td>" . $school->name . "</td>";
-                    echo "<td> <a href=\"school/" . $school->id . "\" class=\"btn btn-success\"> Explorar</a> </td>";
-                    echo "</tr>";
+                    foreach ($schools as $school) 
+                    {
+                        echo "<tr>";
+                        echo "<th scope=\"row\">" . $school->id . "</th>";
+                        echo "<td>" . $school->name . "</td>";
+                        echo "<td> <a href=\"school/" . $school->id . "\" class=\"btn btn-success\"> Explorar</a> </td>";
+                        echo "</tr>";
+                    }
                 }
-            }
-            ?>
-        </tbody>
-    </table>
+                ?>
+            </tbody>
+        </table>
 
-    @else
-        <br>
-        <div class="jumbotron"  align="center">
-        <br>
-        <h6 class="display-6"> Áreas de la Universidad Nacional de San Agustín </h6>
-        <br><br>
-        <a href="link_repository/ing" class="btn btn-success btn-lg">Ingenierías</a>
-        <br><br>
-        <a href="link_repository/bio" class="btn btn-warning btn-lg">Biomédicas</a>
-        <br><br>
-        <a href="link_repository/soc" class="btn btn-danger btn-lg">Sociales</a>
-        <br><br>
-        </div>
-        <br>
-    @endif  
+        @else
+            <br>
+            <div class="jumbotron"  align="center">
+            <br>
+            <h6 class="display-6"> Áreas de la Universidad Nacional de San Agustín </h6>
+            <br><br>
+            <a href="link_repository/ing" class="btn btn-success btn-lg">Ingenierías</a>
+            <br><br>
+            <a href="link_repository/bio" class="btn btn-warning btn-lg">Biomédicas</a>
+            <br><br>
+            <a href="link_repository/soc" class="btn btn-danger btn-lg">Sociales</a>
+            <br><br>
+            </div>
+            <br>
+        @endif  
 
-</div>
+    </div>
 
 @endsection
