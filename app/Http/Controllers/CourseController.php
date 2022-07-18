fe871c89006f8b0adefddc6098fd162d2ed43f15
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2022-07-18 09:20:43 -0500

    feat: check if the course belongs to my school

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Course;
 use App\Models\Resource;
 
@@ -14,7 +15,20 @@ class CourseController extends Controller
         $course = Course::where('id', $id)->get()->first();
         $origin = 'Recursos de ' . Course::find($id)->name;
         $resources = Resource::where('course_id', $id)->get();
-        return view('course', compact('course', 'origin', 'resources'));
+        
+        // verify if the course belongs to my school
+        $my_school = DB::table('course_school')
+            ->join('schools', 'schools.id', '=', 'course_school.school_id')
+            ->join('user_school', 'user_school.school_id', '=', 'schools.id')
+            ->where('course_school.course_id', $id)
+            ->where('user_school.user_id', auth()->user()->id);
+
+        $is_my_course = false;
+        if ($my_school->count() > 0) {
+            $is_my_course = true;
+        }
+
+        return view('course', compact('course', 'origin', 'resources', 'is_my_course'));
     }
 
     public function resource($id)
