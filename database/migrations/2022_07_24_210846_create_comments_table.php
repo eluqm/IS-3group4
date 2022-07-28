b7a750c9bd45c0b843c5c3698c6bf761f7e8f524
LuisSante <lsantet@unsa.edu.pe>
2022-07-27 22:22:28 -0500

    feat: add new table for Comments

---
+++ database/migrations/2022_07_24_210846_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment', 5000);
+            $table->integer('teacher_id');
+            $table->integer('rating');
+            $table->string('adjective', 20);
+            $table->timestamps();
+            $table->integer('made_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
