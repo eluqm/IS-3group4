cddb4efd470ecb98e4a4e106b64dd551f73523d8
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-07-29 20:27:59 -0500

    chore: all seeders calls moved to general database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        $this->call(CourseSeeder::class);
+        $this->call(SchoolSeeder::class);
+        $this->call(CourseSchoolSeeder::class);
     }
 }
