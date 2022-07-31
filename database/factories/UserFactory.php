f8720c4eb70e2b0ce244eb09f185a92cb3646155
MaryoriHilares <mhilaresa@unsa.edu.pe>
2022-07-31 08:50:16 -0500

    feat: modified UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,16 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'id' => $this->faker->unique()->numberBetween(1, 1000),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'user_type' => $this->faker->numberBetween(0, 3),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
