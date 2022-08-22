b211fa65cf7ef7ba412a9da784cc53c530b6a4c7
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-08-22 09:47:40 -0500

    test: Renamed test

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -5,14 +5,14 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class HomeTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_home_page_displays_home_view()
     {
         $response = $this->get('/');
 
